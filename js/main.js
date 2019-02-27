ea6ce2714cea63011deb236ee10c57fe6f7f2e00
KEYHAN Akbarzadeh <keyhan.igo@gmail.com>
2019-02-27 21:10:44 +0300

    url fix + images folder added

--- js/main.js
+++ js/main.js
@@ -9,9 +9,12 @@ app.controller('myAppController', ['$scope', function($scope) {
 
 }]);
 
-app.config(function($routeProvider) {
+
+app.config(function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
       templateUrl: '/home.html'
     })
+
+    $locationProvider.html5Mode(true);
 });
